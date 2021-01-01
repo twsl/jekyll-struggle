660050da2a6defd1f50aa8f3d9594af677ea4e77
twsl <45483159+twsl@users.noreply.github.com>
2021-01-01 22:47:13 +0100

    cookie

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
   AOS.init({
     startEvent: "load",
   });
+
+  theme();
 });
 
 $(window).on("load", function () {
@@ -22,36 +24,72 @@ window.addEventListener("load", function (event) {
 
 $(function () {
   console.log("document loaded");
-
-  theme();
+  handleScroll();
 
 });
+
+function handleScroll() {
+  // $(window).scroll(function () {
+  //   if ($(document).scrollTop() > 50) {
+  //     $('nav').addClass('transparent');
+  //   } else {
+  //     $('nav').removeClass('transparent');
+  //   }
+  // });
+  window.onscroll = function () {
+    let isScrolled = false
+    const scrollPoint = 100
+    const nav = $('#mainNav')
+    const scroll = $('.scroll-top')
+
+    function onScroll() {
+      if (window.pageYOffset > scrollPoint && !isScrolled) {
+        nav.removeClass('navbar-dark').addClass('navbar-light');
+        scroll.addClass('scroll-top-visible');
+        isScrolled = true
+      } else if (window.pageYOffset <= scrollPoint && isScrolled) {
+        nav.removeClass('navbar-light').addClass('navbar-dark');
+        scroll.removeClass('scroll-top-visible');
+        isScrolled = false
+      }
+    }
+
+    onScroll()  // Makes sure that the class is attached on the first render
+    return onScroll
+  }()
+}
 
 function theme() {
   const darkSwitch = $('.theme-button');
   if (darkSwitch) {
-    const darkThemeSelected =
-      localStorage.getItem('darkSwitch') !== null &&
-      localStorage.getItem('darkSwitch') === 'dark';
+    const darkThemeSelected = localStorage.getItem('darkSwitch') !== null && localStorage.getItem('darkSwitch') === 'dark';
     darkSwitch.checked = darkThemeSelected;
     darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') :
       document.body.removeAttribute('data-theme');
 
     $('.theme-button').on('click', function () {
-      if ($(this).find('svg').attr('data-icon') == 'moon' && $(this).find('svg').attr('data-prefix') == 'fas') {
-        $(this).find('svg').attr('data-icon', 'moon');
-        $(this).find('svg').attr('data-prefix', 'far');
 
+      if (toggleFontawesomeIcon(darkSwitch, 'fas', 'far', 'moon', 'moon')) {
         document.body.setAttribute('data-theme', 'dark');
         localStorage.setItem('darkSwitch', 'dark');
-      } else {
-        $(this).find('svg').attr('data-icon', 'moon');
-        $(this).find('svg').attr('data-prefix', 'fas');
-
+      }
+      else {
         document.body.removeAttribute('data-theme');
         localStorage.removeItem('darkSwitch');
-      };
-      resetTheme();
+      }
     });
   }
+}
+
+function toggleFontawesomeIcon(elem, styleOld, styleNew, iconOld, iconNew) {
+  if (elem.find('svg').attr('data-icon') == iconOld
+    && elem.find('svg').attr('data-prefix') == styleOld) {
+    elem.find('svg').attr('data-icon', iconNew);
+    elem.find('svg').attr('data-prefix', styleNew);
+    return true;
+  } else {
+    elem.find('svg').attr('data-icon', iconOld);
+    elem.find('svg').attr('data-prefix', styleOld);
+    return false;
+  };
 }
